e80e417cf283e521fc6e5430fdad1e7c24c6cb20
OksanaKuzich <komoks255@gmail.com>
2023-01-12 21:13:25 +0200

    add functions removeContact(contactId) and addContact(name, email, phone)

--- contacts.js
+++ contacts.js
@@ -1,5 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.resolve("./db/contacts.json");
 
@@ -23,6 +24,27 @@ async function getContactById(contactId) {
   }
 }
 
-function removeContact(contactId) {}
+async function removeContact(contactId) {
+  try {
+    const allContacts = await listContacts();
+    const filterContacts = allContacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    console.log(filterContacts);
+    fs.writeFile(contactsPath, JSON.stringify(filterContacts));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-function addContact(name, email, phone) {}
+async function addContact(name, email, phone) {
+  try {
+    const allContacts = await listContacts();
+    const user = { id: uuidv4(), name, email, phone };
+    console.log(user);
+    allContacts.push(user);
+    fs.writeFile(contactsPath, JSON.stringify(allContacts));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
