3b9986e80bcc91a86edca184d5d77848540dea38
OksanaKuzich <komoks255@gmail.com>
2023-01-12 19:30:31 +0200

    added function listContacts() and getContactById(contactId)

--- contacts.js
+++ contacts.js
@@ -1,21 +1,28 @@
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
+const fs = require("fs").promises;
+const path = require("path");
 
-// TODO: задокументировать каждую функцию
-function listContacts() {
-  // ...твой код
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parsedData = JSON.parse(data);
+    return parsedData;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  try {
+    const allContacts = await listContacts();
+    const contactById = allContacts.find((contact) => contact.id === contactId);
+    return contactById;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твой код
-}
+function removeContact(contactId) {}
 
-function addContact(name, email, phone) {
-  // ...твой код
-}
+function addContact(name, email, phone) {}
